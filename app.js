e16e6fe2a07e59a9d2f89881fd771f0253c4e7ce
Judah-fuego <145150051+Judah-fuego@users.noreply.github.com>
2024-03-17 21:05:25 +0300

    Update app.js

--- app.js
+++ app.js
@@ -11,3 +11,22 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 const hiddenElements = document.querySelectorAll('.wrapSection');
+
+let mybutton = document.getElementById("backToTheTopButton");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
